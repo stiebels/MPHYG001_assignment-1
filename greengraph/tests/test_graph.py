336afc8661eea16337a0e855221e34ef58bdd965
stiebels <simon.stiebellehner@gmail.com>
2016-12-06 17:30:45 +0000

    added constructor test to test_graph

--- greengraph/tests/test_graph.py
+++ greengraph/tests/test_graph.py
@@ -8,6 +8,16 @@ from greengraph.Map import Map
 
 t_Graph = Graph('London', 'Cambridge')
 
+
+@patch('geopy.geocoders.GoogleV3')
+def t_Graph_init(m_geocode):
+    # Test initialization values of Graph
+    t_Graph = Graph('London', 'Cambridge')
+    assert (t_Graph.start == 'London')
+    assert (t_Graph.end == 'Cambridge')
+    m_geocode.assert_called_with(domain="maps.google.co.uk")
+
+
 def t_location_sequence():
     # Test calculation of location_sequence
     to_be = np.array([[-10., -10.], [-5., -5.], [0., 0.], [5., 5.], [10., 10.]])
@@ -29,16 +39,16 @@ def t_geolocate():
 
 def t_green_between():
     # Test calculation of green_between
-    m_sequence = [0,0,0,0,93575]
+    m_sequence = [0, 0, 0, 0, 93575]
     with patch.object(Map, '__init__') as m_Map:
         [m_Map.count_green() for location in m_sequence]
         assert m_Map.count_green.call_count == len(m_sequence)
 
 
 
-#def t_coordinates(start=(-181, -50), end=(181, 50), steps=20):
-    # Test possible coordinates
-    # Non-existent coordinates can be used, however, since the GoogleAPI will
-    # respond with 'None', this error is basically treated by function t_geocoder.
-#    with raises(ValueError):
-#        t_Graph.location_sequence(start, end, steps)
+        # def t_coordinates(start=(-181, -50), end=(181, 50), steps=20):
+        # Test possible coordinates
+        # Non-existent coordinates can be used, however, since the GoogleAPI will
+        # respond with 'None', this error is basically treated by function t_geocoder.
+        #    with raises(ValueError):
+        #        t_Graph.location_sequence(start, end, steps)
