581e11a6657d05cada4faf5c696ae2192ace2883
stiebels <simon.stiebellehner@gmail.com>
2016-12-06 12:56:59 +0000

    changed setup.py for testing purpose

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(name = 'Greengraph',
-    version = '0.1',
+    version = '0.2',
     description = 'Calculates the number of green pixels between two geographical locations',
     author = 'Simon Stiebellehbner',
     author_email = 'ucabsti@ucl.ac.uk',
     maintainer = 'Simon Stiebellehner',
     maintainer_email = 'ucabsti@ucl.ac.uk',
     url = 'https://github.com/stiebels/',
-    packages = find_packages(exclude=['*test']),
+    packages = find_packages(),
     license = 'MIT',
     install_requires = ['numpy', 'matplotlib', 'geopy', 'requests', 'argparse'],
 	scripts= ['scripts/run_greengraph.py']
