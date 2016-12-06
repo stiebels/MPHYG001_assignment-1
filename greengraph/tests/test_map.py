2e877c8133231a9af639cd97d268e2a8c964109e
stiebels <simon.stiebellehner@gmail.com>
2016-12-06 12:00:41 +0000

    added fixture-define function to test_map; added new fixture

--- greengraph/tests/test_map.py
+++ greengraph/tests/test_map.py
@@ -2,19 +2,31 @@ from mock import Mock, patch, MagicMock
 import numpy as np
 from greengraph.Map import Map
 from PIL import Image
+from os import listdir
 
 
-directory = '/home/sist/PycharmProjects/MPHYG001_assignment-1/greengraph/tests/fixtures/'
-m_img = Image.open(directory + 'london_map.png')
-m_green = Image.open(directory + 'green.png')
+def define_img_fixtures(files):
+    directory = '/home/sist/PycharmProjects/MPHYG001_assignment-1/greengraph/tests/fixtures/'
+    m_imgs_fix = []
+
+    for file in os.listdir(directory):
+        byteImgIO = BytesIO()
+        m_img = Image.open(directory + str(file))
+        m_img.save(byteImgIO, "PNG")
+        byteImgIO.seek(0)
+        m_imgs_fix.append(byteImgIO.read())
+
+    return m_imgs_fix
 
 
-# INVALID PNG HEADER ???
-@patch('requests.get', return_value=MagicMock(content=m_img.tobytes()))
+m_imgs_fix = define_img_fixtures(['lc1.png'])
+
+
+@patch('requests.get', return_value=MagicMock(content=m_imgs_fix[0]))
 def t_Map(m_req_get):
     for i in range(0, 2):
         if i == 0:
-            t_Map = Map(-10, -10, satellite=False)
+            t_Map = Map(51.5073509, -0.1277583, satellite=False)  # London
 
             # Check if requests.get is called with right parameters
             m_req_get.assert_called_with(
@@ -23,12 +35,12 @@ def t_Map(m_req_get):
                     sensor=str(False).lower(),
                     zoom=10,
                     size="x".join(map(str, (400, 400))),
-                    center=",".join(map(str, (-10, -10))),
+                    center=",".join(map(str, (51.5073509, -0.1277583))),  # London
                     style="feature:all|element:labels|visibility:off"
                 )
             )
         else:
-            t_Map = Map(-10, -10)
+            t_Map = Map(51.5073509, -0.1277583)  # London
             # Check if requests.get is called with right parameters
             m_req_get.assert_called_with(
                 base="http://maps.googleapis.com/maps/api/staticmap?",
@@ -36,10 +48,10 @@ def t_Map(m_req_get):
                     sensor=str(False).lower(),
                     zoom=10,
                     size="x".join(map(str, (400, 400))),
-                    center=",".join(map(str, (-10, -10))),
+                    center=",".join(map(str, (51.5073509, -0.1277583))),  # London
                     style="feature:all|element:labels|visibility:off",
-                    maptype = "satellite"
-            )
+                    maptype="satellite"
+                )
             )
 
             # Check self.pixels calculation
@@ -48,27 +60,44 @@ def t_Map(m_req_get):
             assert (np.shape(t_Map.pixels) == (400, 400, 3))
 
 
-# INVALID PNG HEADER ???
-@patch('requests.get', return_value=MagicMock(content=m_img.tobytes()))
+@patch('requests.get', return_value=MagicMock(content=m_fix1_img))
 def t_green(m_req_get):
-    t_Map = Map(-10, -10)
+    t_Map = Map(51.5073509, -0.1277583)  # London
 
     # Check that green is recognized/computed correctly
-    thresholds = range(0, 10)
-    check_green = np.array
+    thresholds = np.arange(0.5, 2.5, 0.5)
+    check_green = []
     for number in thresholds:
         check_green.append(sum(sum(t_Map.green(number))))
 
     for i in range(1, len(check_green)):
-        assert ((check_green[i] > check_green[i - 1]) is True)
+        assert ((check_green[i] < check_green[i - 1]) == True)
 
 
 # INVALID PNG HEADER ???
-@patch('requests.get', return_value=MagicMock(content=m_green.tobytes()))
+@patch('requests.get', return_value=MagicMock(content=m_fix1_img))
 def t_count_green(m_req_get):
-    t_Map = Map(-10, -10)
+    t_Map = Map(51.5073509, -0.1277583) # London
 
-    # Check that green and count_green return correct sum
-    # REPLACE X with NUMBER OF GREEN PIXELS
-    assert (t_Map.count_green(1.1) == XXX)
-    assert (sum(sum(t_Map.green(1.1))) == XXX)
+    # Check that green and count_green return correct sums
+    assert (t_Map.count_green(1.1) == 108024)
+    assert (sum(sum(t_Map.green(1.1))) == 108024)
+
+
+# INVALID PNG HEADER ???
+@patch('requests.get', return_value=MagicMock(content=m_fix1_img))
+def t_show_green(m_req_get):
+    t_Map = Map(51.5073509, -0.1277583) # London
+
+    zeros = 0
+    nines = 0
+    for char in t_Map.show_green(t_Map):
+        if '0' in str(char) == 0:
+            zeros += 1
+        if '9' in str(char):
+            nines += 1
+
+    assert (zeros == 3958)
+    assert (nines == 3583)
+    assert (len(t_Map.show_green(t_Map)) == 22283)
+    assert (type(t_Map.show_green(t_Map)) == 'bytes')
