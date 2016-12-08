092285b5aa53672aafb086e02d825b9c8e3ce3f8
stiebels <simon.stiebellehner@gmail.com>
2016-12-08 18:31:17 +0000

    debugged cmd.py

--- greengraph/cmd.py
+++ greengraph/cmd.py
@@ -4,13 +4,14 @@ from greengraph.Graph import Graph
 
 
 def runModule():
-    parser = ArgumentParser(description='Calculates the number of green pixels between two geographical locations.')
-    parser.add_argument('-b', dest='begin', help='Enter start location.', required=True)
-    parser.add_argument('-e', dest='end', help='Enter location of target destination.', required=True)
+    parser = ArgumentParser(description='Generates a graph that displays the number of green pixels between two geographical locations.')
+    parser.add_argument('-b', '--begin', dest='begin', help='Enter start location.', required=True)
+    parser.add_argument('-e', 'end', dest='end', help='Enter location of target destination.', required=True)
     parser.add_argument('-s', default=25, dest='steps', help='Steps between begin and end', required=False)
 
     args = parser.parse_args()
     plotGraph(args.begin, args.end, args.steps)
+
 
 def plotGraph(begin, end, steps):
     mygraph = Graph(begin, end)
