5b603949fc5c8346263f60183b64dd14dc1a6924
stiebels <simon.stiebellehner@gmail.com>
2016-12-08 18:06:44 +0000

    extended command line interface description

--- greengraph/cmd.py
+++ greengraph/cmd.py
@@ -7,17 +7,15 @@ def runModule():
     parser = ArgumentParser(description='Calculates the number of green pixels between two geographical locations.')
     parser.add_argument('-b', dest='begin', help='Enter start location.', required=True)
     parser.add_argument('-e', dest='end', help='Enter location of target destination.', required=True)
-    parser.add_argument('-s', default=25, dest='steps', help='Steps/points between begin and end', required=False)
+    parser.add_argument('-s', default=25, dest='steps', help='Steps between begin and end', required=False)
 
     args = parser.parse_args()
     plotGraph(args.begin, args.end, args.steps)
-
 
 def plotGraph(begin, end, steps):
     mygraph = Graph(begin, end)
     data = mygraph.green_between(steps)
     plt.plot(data)
-    plt.show()
 
 
 if __name__ == '__main__':
