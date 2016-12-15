7edf0218f6e094b5b0cfda575ad057b6d4a49f39
stiebels <simon.stiebellehner@gmail.com>
2016-12-15 16:49:24 +0000

    adapted cmd test cases to savefig implementation

--- greengraph/tests/test_cmd.py
+++ greengraph/tests/test_cmd.py
@@ -14,11 +14,24 @@ def t_plotGraph_plt():
     with patch.object(Graph, 'green_between') as m_green_between:
         with patch.object(plt, 'plot') as m_plt_plot:
             with patch.object(plt, 'show') as m_plt_show:
-                plotGraph('London', 'Cambridge', 5)
-
-                # Checks if plt functions are called exactly once per run
-                assert (m_plt_plot.call_count == 1)
-                assert (m_plt_show.call_count == 1)
+                with patch.object(plt, 'savefig') as m_plt_save:
+                    for i in range(0,2):
+                        if i == 0: # Checks functions calls if path is not specified
+                            plotGraph('London', 'Cambridge', 5, path=None)
+                            # Checks if plt.plot and plt.show are called exactly once per run
+                            assert (m_plt_plot.call_count == 1)
+                            # Checks whether plt.show is called
+                            assert (m_plt_show.call_count == 1)
+                            # Checks if plt.savefig is not called
+                            assert (m_plt_save.call_count == 0)
+                        if i == 1: # Checks function calls if path is specified
+                            plotGraph('London', 'Cambridge', 5, path='/testdir/test.png')
+                            # Since it's the second run in the loop, the plt.plot function should have been called twice sofar
+                            assert (m_plt_plot.call_count == 2)
+                            # Checks whether plt.show was called again
+                            assert (m_plt_show.call_count == 1)
+                            # Checks whether plt.savefig was called
+                            assert (m_plt_save.call_count == 1)
 
 
 def t_plotGraph_plt_input():
